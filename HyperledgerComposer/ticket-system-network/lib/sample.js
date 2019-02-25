19bbdfac4874023e6e4a97418820e7c96b2ca675
devonrudd <devon.rudd@jessup.edu>
2019-02-24 22:24:10 -0800

    Changed namespace to openticket

--- HyperledgerComposer/ticket-system-network/lib/sample.js
+++ HyperledgerComposer/ticket-system-network/lib/sample.js
@@ -16,7 +16,7 @@
 
 /**
  * Sample transaction processor function.
- * @param {org.example.basic.SampleTransaction} tx The sample transaction instance.
+ * @param {org.openticket.SampleTransaction} tx The sample transaction instance.
  * @transaction
  */
 async function sampleTransaction(tx) {  // eslint-disable-line no-unused-vars
@@ -28,12 +28,12 @@ async function sampleTransaction(tx) {  // eslint-disable-line no-unused-vars
     tx.asset.value = tx.newValue;
 
     // Get the asset registry for the asset.
-    const assetRegistry = await getAssetRegistry('org.example.basic.SampleAsset');
+    const assetRegistry = await getAssetRegistry('org.openticket.SampleAsset');
     // Update the asset in the asset registry.
     await assetRegistry.update(tx.asset);
 
     // Emit an event for the modified asset.
-    let event = getFactory().newEvent('org.example.basic', 'SampleEvent');
+    let event = getFactory().newEvent('org.openticket', 'SampleEvent');
     event.asset = tx.asset;
     event.oldValue = oldValue;
     event.newValue = tx.newValue;

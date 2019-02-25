2215c2b198a046a87f479ce6eba767b9b4f3adbc
devonrudd <devon.rudd@jessup.edu>
2019-02-24 23:04:14 -0800

    Updated setupSystem function

--- HyperledgerComposer/ticket-system-network/lib/sample.js
+++ HyperledgerComposer/ticket-system-network/lib/sample.js
@@ -21,6 +21,31 @@
  */
 
  function setupSystem(setupSystem) {
+
     var factory = getFactory();
-    
+    var NS = 'org.openticket';
+
+    // Create Initial Technician
+    var tech = factory.newResource(NS, 'Technician', 'support@org.com');
+    tech.firstName = 'Devon';
+    tech.lastName = 'Rudd';
+
+    // Create Client
+    var client = factory.newResource(NS, 'Client', 'client@org.com');
+    client.firstName = 'Chester';
+    client.lastName = 'Tester';
+
+    // Create Ticket
+    var ticket = factory.newResource(NS, 'Ticket', 'TICK_001');
+    ticket.status = 'OPEN'
+    ticket.subject = 'Example Ticket'
+    ticket.description = 'This is your first ticket. You will see the body of the email received in this section.'
+    ticket.tech = factory.newRelationship(NS, 'Technician', 'support@org.com');
+    ticket.client = factory.newRelationship(NS, 'Client', 'client@org.com');
+    var now = setupSystem.timestamp;
+    ticket.openTime = now
+    ticket.update = now
+
+    // Update Network
+
  }
